2c4f0bd8652b0f8869ad8d07c1f82ef646253c79
creative-paramu <pparameshwari@thirvusoft.in>
2023-12-23 16:02:01 +0530

    perm_level_2

--- jmtech/custom/py/after_migrate.py
+++ jmtech/custom/py/after_migrate.py
@@ -8,6 +8,6 @@ def lead_property_setter():
     make_property_setter("Lead", "status", "options", "Lead\nReplied\nQuotation\nLost Quotation\nInterested\nConverted\nDo Not Contact\nSite Visited", "Small Text")
     
 def user_property_setter():
-    make_property_setter("User", "roles_html", "permlevel", "2", "Data")
+    make_property_setter("User", "roles_html", "permlevel", 2, "Int")
     make_property_setter("User", "module_profile", "default", "CRM", "Small Text")
     make_property_setter("User", "role_profile_name", "default", "CRM", "Small Text")
