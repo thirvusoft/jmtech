ccd173c0b6d803b6b928d838b35e55e73a5e921a
creative-paramu <pparameshwari@thirvusoft.in>
2023-12-23 15:55:16 +0530

    user_permission

--- jmtech/custom/py/after_migrate.py
+++ jmtech/custom/py/after_migrate.py
@@ -2,6 +2,12 @@ import frappe
 from frappe.custom.doctype.property_setter.property_setter import make_property_setter
 def after_migrate():
     lead_property_setter()
+    user_property_setter()
 
 def lead_property_setter():
     make_property_setter("Lead", "status", "options", "Lead\nReplied\nQuotation\nLost Quotation\nInterested\nConverted\nDo Not Contact\nSite Visited", "Small Text")
+    
+def user_property_setter():
+    make_property_setter("User", "roles_html", "permlevel", "2", "Data")
+    make_property_setter("User", "module_profile", "default", "CRM", "Small Text")
+    make_property_setter("User", "role_profile_name", "default", "CRM", "Small Text")
